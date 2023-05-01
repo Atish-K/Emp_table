eb3d3e610973750699a25ab729affd453f291fb0
Atish-K <atish0518@gmail.com>
2023-05-01 23:57:55 +0530

    env

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DB = "mongodb+srv://aatish0075:K4Um6DvUgd7dNCyV@cluster0.0p62kzd.mongodb.net/employee_table?retryWrites=true&w=majority";
+const DB = process.env.DATABASE
 
 
 mongoose.connect(DB,{
